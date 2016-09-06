42c54732c173719ffc83cb65df0b542672eb31fc
Egor Pugin <egor.pugin@gmail.com>
2016-09-06 23:35:42 +0300

    Update main.cpp

--- with_cppan/main.cpp
+++ with_cppan/main.cpp
@@ -1,4 +1,7 @@
 /*
+local_settings:
+  build:
+    silent: false
 dependencies:
   # remove the next line if you need only tesseract dependency
   # e.g. when using other image library - opencv or something else
